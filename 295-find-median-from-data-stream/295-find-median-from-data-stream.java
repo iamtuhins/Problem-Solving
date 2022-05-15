711ffe63f64e4187697319aa605b32dca32076e6
iamtuhins <77203950+iamtuhins@users.noreply.github.com>
2022-05-15 20:53:44 +0530

    Time: 316 ms (8.05%), Space: 125.1 MB (39.78%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.java
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.java
@@ -1,5 +1,7 @@
 class MedianFinder {
+    //to store all the large number and find the smallest among them
     PriorityQueue<Integer>minHeap=new PriorityQueue<>();
+    //to store all the small number and find the largest among them
     PriorityQueue<Integer>maxHeap=new PriorityQueue<>(Collections.reverseOrder());
     boolean even=true;
 
@@ -8,20 +10,26 @@ class MedianFinder {
     }
     
     public void addNum(int num) {
+        //if even and now we need to add one more number then it will be odd 
+        //so i'll add it in maxheap first and than poll out largest amog them and will store it in minHeap.
         if(even){
             maxHeap.add(num);
             minHeap.add(maxHeap.poll());
         }else{
+            //else i'll add it in the minHeap first and then will poll out and store it in the maxHeap
             minHeap.add(num);
             maxHeap.add(minHeap.poll());
         }
+        //will making opposite what it is
         even=!even;
     }
     
     public double findMedian() {
         if(even){
+            //if it is even 
             return (minHeap.peek()+maxHeap.peek())/2.00;
         }
+        //if it's odd 
         return minHeap.peek();
     }
 }
