668c05fb771c4bd93d836b400498c414cb53c359
iamtuhins <77203950+iamtuhins@users.noreply.github.com>
2022-05-18 20:37:00 +0530

    Time: 7 ms (41.89%), Space: 48.3 MB (44.45%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -1,28 +1,31 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         int n=nums.length;
-        List<Integer>out=new ArrayList<>();
-        List<Integer>num=new ArrayList<>();
+        //map will hold all the numbers occurances firstly
+        Map<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
         List<List<Integer>>sol=new ArrayList<>();
-        fn(sol,out,num,nums);
+        List<Integer>out=new ArrayList<>();
+        fn(sol,nums,map,out);
         return sol;
         
         
     }
-    void fn(List<List<Integer>>sol,List<Integer>out,List<Integer>num,int []nums){
+    void fn(List<List<Integer>>sol,int nums[],Map<Integer,Integer>map,List<Integer>out){
         if(out.size()>=nums.length){
-            if(!sol.contains(out)){
-                sol.add(new ArrayList<>(out));
-            }
+            sol.add(new ArrayList<>(out));
             return;
         }
-        for(int i=0;i<nums.length;i++){
-            if(!num.contains(i+1)){
-                num.add(i+1);
-                out.add(nums[i]);
-                fn(sol,out,num,nums);
-                num.remove(num.size()-1);
+        for(int i:map.keySet()){
+            //will check the count of number if greater than 0 than only will add
+            if(map.get(i)>0){
+                out.add(i);
+                map.put(i,map.get(i)-1);
+                fn(sol,nums,map,out);
                 out.remove(out.size()-1);
+                map.put(i,map.get(i)+1);
             }
         }
     }
