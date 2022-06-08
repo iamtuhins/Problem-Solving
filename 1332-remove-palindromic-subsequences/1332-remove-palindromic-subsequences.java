11f0e6dc83d2e8ba442b0b44517e322bfb57b052
iamtuhins <77203950+iamtuhins@users.noreply.github.com>
2022-06-08 21:42:30 +0530

    Time: 0 ms (100.00%), Space: 42 MB (43.06%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.java
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.java
@@ -1,8 +1,10 @@
 class Solution {
     public int removePalindromeSub(String s) {
         int n=s.length();
+        //All the String are either a or b
         if(palind(s,n-1)){
             return 1;
+            //if full string in palindrom than it will take 1 rounds 
         }
         return 2;
         
