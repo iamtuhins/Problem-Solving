860ce45172a722a8a0a86285e65d0523aa016fb0
iamtuhins <77203950+iamtuhins@users.noreply.github.com>
2022-06-21 22:19:37 +0530

    Time: 129 ms (16.69%), Space: 43 MB (89.89%) - LeetHub

--- 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.java
+++ 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.java
@@ -1,28 +1,23 @@
 class Solution {
     public String[] findRestaurant(String[] list1, String[] list2) {
-        List<String>temp=new ArrayList<>();
-        int n=list1.length;
-        int m=list2.length;
-        int size=Integer.MAX_VALUE;
-        for(int i=0;i<n;i++){
-            String t1=list1[i];
-            for(int j=0;j<m;j++){
-                String t2=list2[j];
-                if(t1.equals(t2) && size>=(i+j)){
-                    if(temp.size()>=1 && size>(i+j)){
-                        temp.remove(temp.size()-1);
+        List<String>sol=new ArrayList<>();
+        int sz=Integer.MAX_VALUE;
+        for(int i=0;i<list1.length;i++){
+            String temp=list1[i];
+            for(int j=0;j<list2.length;j++){
+                if(temp.equals(list2[j]) && sz>=(i+j)){
+                    if(sz>(i+j) && sol.size()>=1){
+                        sol.remove(sol.size()-1);
                     }
-                    temp.add(list1[i]);
-                    size=i+j;
+                    sol.add(list1[i]);
+                    sz=(i+j);
                 }
             }
         }
-        int j=temp.size();
-        String []sol=new String[j];
-        for(int i=0;i<j;i++){
-            sol[i]=temp.get(i);
+        String[] solutions=new String[sol.size()];
+        for(int i=0;i<solutions.length;i++){
+            solutions[i]=sol.get(i);
         }
-        return sol;
-        
+        return solutions;
     }
 }
