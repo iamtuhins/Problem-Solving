48c6ed3e5febaa82c92d6374799213eb858818c8
iamtuhins <77203950+iamtuhins@users.noreply.github.com>
2022-06-25 21:50:14 +0530

    Time: 2 ms (30.74%), Space: 53.8 MB (53.95%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.java
+++ 665-non-decreasing-array/665-non-decreasing-array.java
@@ -4,9 +4,11 @@ class Solution {
         boolean fl=false;
         for(int i=1;i<n;i++){
             if(nums[i-1]>nums[i] && fl==false){
+                //if nums are large, large,large than make it large also.
                 if(i>=2 && nums[i-2]>nums[i]){
                     nums[i]=nums[i-1];
                 }else{
+                    //if nums are small, large, small than make the large small.
                     nums[i-1]=nums[i];
                 }
                 fl=true;
