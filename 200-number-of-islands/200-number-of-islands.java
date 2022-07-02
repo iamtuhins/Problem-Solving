0c938baa12f7d83db700dc0a04dc58c8b1067b57
iamtuhins <77203950+iamtuhins@users.noreply.github.com>
2022-07-02 21:39:29 +0530

    Time: 7 ms (33.66%), Space: 57.4 MB (46.96%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -2,12 +2,12 @@ class Solution {
     public int numIslands(char[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
-        boolean vist[][]=new boolean[n][m];
+        int vist[][]=new int[n][m];
         int sol=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]=='1' && vist[i][j]==false){
-                    drawTree(i,j,vist,grid);
+                if(vist[i][j]==0 && grid[i][j]=='1'){
+                    dfs(i,j,vist,grid);
                     sol++;
                 }
             }
@@ -15,15 +15,14 @@ class Solution {
         return sol;
         
     }
-    void drawTree(int i,int j,boolean vist[][],char grid[][]){
-        if(i<0 || i>=grid.length ||j<0 || 
-           j>=grid[0].length || grid[i][j]=='0' || vist[i][j]==true){
-           return; 
+    void dfs(int i, int j,int [][]vist,char [][]grid){
+        if(i<0 ||j<0 ||i>=grid.length || j>=grid[0].length||vist[i][j]==1 || grid[i][j]=='0'){
+            return;
         }
-        vist[i][j]=true;
-        drawTree(i+1,j,vist,grid);
-        drawTree(i,j+1,vist,grid);
-        drawTree(i-1,j,vist,grid);
-        drawTree(i,j-1,vist,grid);
+        vist[i][j]=1;
+        dfs(i+1,j,vist,grid);
+        dfs(i,j+1,vist,grid);
+        dfs(i-1,j,vist,grid);
+        dfs(i,j-1,vist,grid);
     }
 }
