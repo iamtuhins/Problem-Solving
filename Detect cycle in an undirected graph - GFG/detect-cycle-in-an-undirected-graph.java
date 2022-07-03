c22b956a7a9d31c6806e2ef887c9790516b660ba
iamtuhins <77203950+iamtuhins@users.noreply.github.com>
2022-07-03 21:29:55 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -37,46 +37,30 @@ class Solution {
         // Code here
         int vist[]=new int[V];
         Map<Integer,Integer>map=new HashMap<>();
-        boolean sol[]=new boolean[1];
+        boolean sol=false;
         for(int i=0;i<V;i++){
             if(vist[i]==0){
                 map.put(i,-1);
-                dfs(adj,i,sol,map,vist);
-            if(sol[0]==true){
-                return true;
-            }
+                sol=dfs(adj,vist,map,i);
+                if(sol==true){
+                    return true;
+                }
             }
         }
-        return false;
+        return sol;
     }
-    void dfs(ArrayList<ArrayList<Integer>>adj,int node,boolean arr[],
-    Map<Integer,Integer>map,int vist[]){
+    boolean dfs(ArrayList<ArrayList<Integer>>adj,int []vist,Map<Integer,Integer>map,int node){
         vist[node]=1;
         for(int n: adj.get(node)){
             if(vist[n]==1 && map.get(node)!=n){
-                arr[0]=true;
-                return;
+                return true;
             }else if(vist[n]==0){
+                boolean temp=false;
                 map.put(n,node);
-                dfs(adj,n,arr,map,vist);
-            }
-        }
-    }
-    boolean bfs(int node,ArrayList<ArrayList<Integer>>adj,int[]vist,Map<Integer,Integer>map){
-        Queue<Integer>q=new LinkedList<>();
-        q.add(node);
-        vist[node]=1;
-        map.put(node,-1);
-        while(!q.isEmpty()){
-            int n=q.remove();
-            for(int m: adj.get(n)){
-                if(vist[m]==1 && map.get(n)!=m){
-                    return true;
-                }else if(vist[m]==0){
-                    vist[m]=1;
-                    q.add(m);
-                    map.put(m,n);
-                }
+                 temp=dfs(adj,vist,map,n);
+                 if(temp){
+                     return temp;
+                 }
             }
         }
         return false;
