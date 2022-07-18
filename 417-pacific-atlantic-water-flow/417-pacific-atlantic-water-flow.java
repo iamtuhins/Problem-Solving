179dc4b4c8de0a5252c41c7b0eb49e9a95442ccf
iamtuhins <77203950+iamtuhins@users.noreply.github.com>
2022-07-18 16:54:26 +0530

    Time: 4 ms (99.30%), Space: 43.7 MB (87.44%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
@@ -2,41 +2,38 @@ class Solution {
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         int n=heights.length;
         int m=heights[0].length;
-        List<List<Integer>>sol=new ArrayList<>();
-        Set<List<Integer>>po=new HashSet<>();
-        Set<List<Integer>>ao=new HashSet<>();
-        for(int j=0;j<m;j++){
-            dfs(heights,po,0,j,heights[0][j]);
-            dfs(heights,ao,n-1,j,heights[n-1][j]);
+        boolean pecefic[][]=new boolean[n][m];
+        boolean atln[][]=new boolean[n][m];
+        for(int i=0;i<m;i++){
+            fn(pecefic,0,i,heights,heights[0][i]);
+            fn(atln,n-1,i,heights,heights[n-1][i]);
         }
         for(int i=0;i<n;i++){
-            dfs(heights,po,i,0,heights[i][0]);
-            dfs(heights,ao,i,m-1,heights[i][m-1]);
+            fn(pecefic,i,0,heights,heights[i][0]);
+            fn(atln,i,m-1,heights,heights[i][m-1]);
         }
+        List<List<Integer>>sol=new ArrayList<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                List<Integer>temp=new ArrayList<>();
-                temp.add(i);
-                temp.add(j);
-                if(po.contains(temp) && ao.contains(temp)){
-                    sol.add(temp);
+                if(pecefic[i][j]==true && atln[i][j]==true){
+                    List<Integer>temp=new ArrayList<>();
+                    temp.add(i);
+                    temp.add(j);
+                    sol.add(new ArrayList<>(temp));
                 }
             }
         }
         return sol;
         
     }
-    void dfs(int [][]ht,Set<List<Integer>>vist,int i,int j,int prev){
-        List<Integer>ls=new ArrayList<>();
-        ls.add(i);
-        ls.add(j);
-        if(i<0 ||j<0 ||i>=ht.length ||j>=ht[0].length||vist.contains(ls) ||prev>ht[i][j]){
+    void fn(boolean [][]arr,int i,int j,int heights[][],int prev){
+        if(i<0 ||j<0 ||i>=heights.length ||j>=heights[0].length ||arr[i][j]==true|prev>heights[i][j]){
             return;
         }
-        vist.add(ls);
-        dfs(ht,vist,i-1,j,ht[i][j]);
-        dfs(ht,vist,i,j-1,ht[i][j]);
-        dfs(ht,vist,i+1,j,ht[i][j]);
-        dfs(ht,vist,i,j+1,ht[i][j]);
+        arr[i][j]=true;
+        fn(arr,i+1,j,heights,heights[i][j]);
+        fn(arr,i,j+1,heights,heights[i][j]);
+        fn(arr,i-1,j,heights,heights[i][j]);
+        fn(arr,i,j-1,heights,heights[i][j]);
     }
 }
