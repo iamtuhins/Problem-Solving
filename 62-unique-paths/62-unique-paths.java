247a85cfc0abd7243dd6baa8df40f88cdad2360e
iamtuhins <77203950+iamtuhins@users.noreply.github.com>
2022-08-02 20:58:27 +0530

    Time: 1 ms (55.29%), Space: 40.7 MB (71.76%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,22 +1,28 @@
 class Solution {
-    public int uniquePaths(int m, int n) {
-        int dp[][]=new int[m][n];
-        int sol=fn(dp,0,0,m,n);
+    public int uniquePaths(int m, int n) { 
+        int num[][]=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                num[i][j]=-1;
+            }
+        }
+        int sol=fn(num,0,0,m,n);
         return sol;
     }
-    int fn(int dp[][],int n,int m,int n1,int m1){
+    int fn(int num[][],int n,int m,int n1,int m1){
         if(n>=n1 || m>=m1){
             return 0;
         }
-        if(dp[n][m]!=0){
-            return dp[n][m];
+        if(num[n][m]>-1){
+            return num[n][m];
         }
         if(n==n1-1 && m==m1-1){
             return 1;
         }
-        int t1=fn(dp,n+1,m,n1,m1);
-        int t2=fn(dp,n,m+1,n1,m1);
-        dp[n][m]=t1+t2;
-        return dp[n][m];
+        int nm1=fn(num,n+1,m,n1,m1);
+        int nm2=fn(num,n,m+1,n1,m1);
+        num[n][m]=nm1+nm2;
+        return nm1+nm2;
+        
     }
 }
