59e0fd7faa6dead03489f940bde3050a18863d84
iamtuhins <77203950+iamtuhins@users.noreply.github.com>
2022-08-06 20:41:31 +0530

    Time: 2 ms (58.15%), Space: 53.6 MB (57.92%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,35 +1,40 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
         int n=graph.length;
-        int arr[]=new int[n];
+        int vist[]=new int[n];
+        Arrays.fill(vist,-1);
         for(int i=0;i<n;i++){
-            arr[i]=-1;
-        }
-        for(int i=0;i<n;i++){
-            if(arr[i]==-1){
-                arr[i]=1;
-                boolean tm=fn(graph,i,arr);
-                if(tm==false){
-                    return false;
+            if(vist[i]==-1){
+                Queue<Pair>q=new LinkedList<>();
+                vist[i]=1;
+                q.add(new Pair(i,1));
+                while(!q.isEmpty()){
+                    Pair p=q.remove();
+                    int id=p.id;
+                    int nums=p.nums;
+                    for(int temp: graph[id]){
+                        if(vist[temp]==-1){
+                            int m2=1-nums;
+                            vist[temp]=m2;
+                            Pair pt=new Pair(temp,m2);
+                            q.add(pt);
+                        }else if(vist[temp]==nums){
+                            return false;
+                        }
+                    }
                 }
             }
+            
         }
         return true;
         
     }
-    boolean fn(int graph[][],int i,int []arr){
-        for(int nums: graph[i]){
-            if(arr[nums]==-1){
-                arr[nums]=1-arr[i];
-                boolean tm=fn(graph,nums,arr);
-                if(tm==false){
-                    return false;
-                }
-            }else if(arr[nums]==arr[i]){
-                return false;
-            }
-        }
-        return true;
-        
+}
+class Pair{
+    int id;
+    int nums;
+    Pair(int n,int m){
+        this.id=n;
+        this.nums=m;
     }
 }
