06b766de50ab14418d38ba9fd41e487b0cdd4f6e
iamtuhins <77203950+iamtuhins@users.noreply.github.com>
2022-08-07 22:00:39 +0530

    Time: 1 ms (89.95%), Space: 54.6 MB (5.50%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,40 +1,31 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
         int n=graph.length;
-        int vist[]=new int[n];
-        Arrays.fill(vist,-1);
+        int colorGraph[]=new int[n];
+        Arrays.fill(colorGraph,-1);
         for(int i=0;i<n;i++){
-            if(vist[i]==-1){
-                Queue<Pair>q=new LinkedList<>();
-                vist[i]=1;
-                q.add(new Pair(i,1));
-                while(!q.isEmpty()){
-                    Pair p=q.remove();
-                    int id=p.id;
-                    int nums=p.nums;
-                    for(int temp: graph[id]){
-                        if(vist[temp]==-1){
-                            int m2=1-nums;
-                            vist[temp]=m2;
-                            Pair pt=new Pair(temp,m2);
-                            q.add(pt);
-                        }else if(vist[temp]==nums){
-                            return false;
-                        }
-                    }
+            if(colorGraph[i]==-1){
+                colorGraph[i]=1;
+                boolean temp=fn(graph,i,colorGraph);
+                if(temp==false){
+                    return false;
                 }
             }
-            
         }
         return true;
-        
     }
-}
-class Pair{
-    int id;
-    int nums;
-    Pair(int n,int m){
-        this.id=n;
-        this.nums=m;
+    boolean fn(int graph[][],int id,int []arr){
+        for(int m: graph[id]){
+            if(arr[m]==-1){
+                arr[m]=1-arr[id];
+                boolean tm=fn(graph,m,arr);
+                if(tm==false){
+                    return false;
+                }
+            }else if(arr[m]==arr[id]){
+                return false;
+            }
+        }
+        return true;
     }
 }
