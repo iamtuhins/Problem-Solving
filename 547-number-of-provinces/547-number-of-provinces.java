eca7343e5db42201e06a351de48b42777e77aecd
iamtuhins <77203950+iamtuhins@users.noreply.github.com>
2022-08-11 20:21:57 +0530

    Time: 6 ms (24.00%), Space: 54.4 MB (29.43%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.java
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -7,6 +7,8 @@ class Solution {
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
+                if(i==j)
+                    continue;
                 if(isConnected[i][j]==1){
                     adj.get(i+1).add(j+1);
                 }
