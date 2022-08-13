abdb7c20fe16e98823d0c60baee278484e77ad20
iamtuhins <77203950+iamtuhins@users.noreply.github.com>
2022-08-13 18:34:45 +0530

    Time: 2 ms (96.29%), Space: 43 MB (51.98%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -1,51 +1,44 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        if(grid==null && grid.length==0){
-            return -1;
-        }
         int n=grid.length;
         int m=grid[0].length;
-        int tot=0;
         Queue<Pair>q=new LinkedList<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]!=0){
-                    tot++;
-                }
                 if(grid[i][j]==2){
                     q.add(new Pair(i,j));
                 }
             }
         }
-        int id_I[]={0,0,-1,1};
-        int id_j[]={1,-1,0,0};
-        int count=0;
-        int time=0;
+        int sol=0;
+        int dist[][]={{0,1},{1,0},{-1,0},{0,-1}};
         while(!q.isEmpty()){
-            int k=q.size();
-            count=count+k;
-            for(int i=0;i<k;i++){
-                Pair p=q.remove();
-                int id_n=p.n;
-                int id_m=p.m;
+            int sz=q.size();
+            for(int i=0;i<sz;i++){
+                Pair temp=q.remove();
                 for(int j=0;j<4;j++){
-                    int n_id=id_n+id_I[j];
-                    int m_id=id_m+id_j[j];
-                    if(n_id<0 ||n_id>=n ||m_id<0 ||m_id>=m ||grid[n_id][m_id]!=1){
-                        continue;
+                    int idn=temp.n;
+                    int idm=temp.m;
+                    idn=idn+dist[j][0];
+                    idm=idm+dist[j][1];
+                    if(idn>=0 && idm>=0 && idn<grid.length && idm<grid[0].length && grid[idn][idm]==1){
+                        q.add(new Pair(idn,idm));
+                        grid[idn][idm]=-1;
                     }
-                    grid[n_id][m_id]=2;
-                    q.add(new Pair(n_id,m_id));
                 }
             }
             if(q.size()>0){
-                time++;
+                sol++;
             }
         }
-        if(count==tot){
-            return time;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    return -1;
+                }
+            }
         }
-        return -1;
+        return sol;
         
     }
 }
