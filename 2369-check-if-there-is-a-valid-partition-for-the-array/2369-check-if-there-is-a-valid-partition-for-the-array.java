451b3394292fb7bc7d08f28b458b943eb700767b
iamtuhins <77203950+iamtuhins@users.noreply.github.com>
2022-08-17 15:38:30 +0530

    Time: 12 ms (54.56%), Space: 60.3 MB (76.85%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
@@ -1,42 +1,42 @@
 class Solution {
     public boolean validPartition(int[] nums) {
         int n=nums.length;
-        int dp[]=new int[n];
+        int map[]=new int[n];
         for(int i=0;i<n;i++){
-            dp[i]=-1;
+            map[i]=-1;
         }
-        return fn(nums,0,dp);
+        boolean sol;
+        sol=fn(nums,0,map);
+        return sol;
         
         
     }
-    boolean fn(int nums[],int id,int dp[]){
-        if(id>=nums.length){
+    boolean fn(int nums[],int id,int[]map){
+        if(id>=nums.length)
             return true;
-        }
-        if(dp[id]!=-1){
-            if(dp[id]==1){
+        if(map[id]>-1){
+            if(map[id]==1)
                 return true;
-            }
-            return false;
+            else
+                return false;
         }
-        boolean fm=false;
-        boolean sm=false;
-        boolean tm=false;
+        boolean fp=false;
+        boolean sp=false;
+        boolean tp=false;
         if(nums.length>id+1 && nums[id]==nums[id+1]){
-            fm=fn(nums,id+2,dp);
+            fp=fn(nums,id+2,map);
         }
-        if(nums.length>id+2 && nums[id]==nums[id+1] && nums[id]==nums[id+2]){
-            sm=fn(nums,id+3,dp);
+        if(nums.length>id+2 && nums[id]==nums[id+1] && nums[id+1]==nums[id+2]){
+            sp=fn(nums,id+3,map);
         }
-        if(nums.length>id+2 && nums[id]==nums[id+1]-1 && nums[id+1]==nums[id+2]-1){
-            tm=fn(nums,id+3,dp);
+        if(nums.length>id+2 && (nums[id]==nums[id+1]-1) && (nums[id+1]==nums[id+2]-1)){
+            tp=fn(nums,id+3,map);
         }
-        if(fm || sm || tm){
-            dp[id]=1;
+        if(fp || sp || tp){
+            map[id]=1;
             return true;
         }
-        dp[id]=0;
+        map[id]=0;
         return false;
-        
     }
 }
