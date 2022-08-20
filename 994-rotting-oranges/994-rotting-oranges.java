c255d210130161abc470a275bacaf19efdd505c6
iamtuhins <77203950+iamtuhins@users.noreply.github.com>
2022-08-20 22:06:17 +0530

    Time: 2 ms (96.42%), Space: 41.7 MB (96.98%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -2,7 +2,9 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
+        int dirt[][]={{0,1},{1,0},{0,-1},{-1,0}};
         Queue<Pair>q=new LinkedList<>();
+        int sol=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
@@ -10,20 +12,20 @@ class Solution {
                 }
             }
         }
-        int sol=0;
-        int dist[][]={{0,1},{1,0},{-1,0},{0,-1}};
         while(!q.isEmpty()){
             int sz=q.size();
             for(int i=0;i<sz;i++){
-                Pair temp=q.remove();
+                Pair p=q.remove();
+                int nowRow=p.n;
+                int nowColm=p.m;
                 for(int j=0;j<4;j++){
-                    int idn=temp.n;
-                    int idm=temp.m;
-                    idn=idn+dist[j][0];
-                    idm=idm+dist[j][1];
-                    if(idn>=0 && idm>=0 && idn<grid.length && idm<grid[0].length && grid[idn][idm]==1){
-                        q.add(new Pair(idn,idm));
-                        grid[idn][idm]=-1;
+                    int putRow=nowRow+dirt[j][0];
+                    int putColm=nowColm+dirt[j][1];
+                    if(putRow>=0 && putColm>=0 && putRow<n && putColm<m){
+                        if(grid[putRow][putColm]==1){
+                            grid[putRow][putColm]=2;
+                            q.add(new Pair(putRow,putColm));
+                        }
                     }
                 }
             }
