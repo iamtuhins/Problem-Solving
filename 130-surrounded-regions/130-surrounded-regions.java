30abc4dfe3c99354beeaa668d2096a85e05507cb
iamtuhins <77203950+iamtuhins@users.noreply.github.com>
2022-08-20 21:47:12 +0530

    Time: 4 ms (35.17%), Space: 51.7 MB (52.57%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.java
+++ 130-surrounded-regions/130-surrounded-regions.java
@@ -2,34 +2,44 @@ class Solution {
     public void solve(char[][] board) {
         int n=board.length;
         int m=board[0].length;
-        for(int i=0;i<n;i++){
-            fn(board,i,0);
-            fn(board,i,m-1);
+        int dirt[][]={{0,1},{1,0},{0,-1},{-1,0}};
+        for(int i=0;i<m;i++){
+            if(board[0][i]=='O'){
+                fn(board,dirt,0,i);
+            }
+            if(board[n-1][i]=='O'){
+                fn(board,dirt,n-1,i);
+            }
         }
-        for(int j=0;j<m;j++){
-            fn(board,0,j);
-            fn(board,n-1,j);
+        for(int i=0;i<n;i++){
+            if(board[i][0]=='O'){
+                fn(board,dirt,i,0);
+            }
+            if(board[i][m-1]=='O'){
+                fn(board,dirt,i,m-1);
+            }
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(board[i][j]=='O'){
-                    board[i][j]='X';
-                }else if(board[i][j]=='T'){
+                if(board[i][j]=='m'){
                     board[i][j]='O';
+                }else if(board[i][j]=='O'){
+                    board[i][j]='X';
                 }
             }
         }
         
     }
-    void fn(char [][]board,int i,int j){
-        if(i<0 || j<0 ||i>=board.length || j>=board[0].length 
-           || board[i][j]=='T' || board[i][j]=='X'){
-            return;
+    void fn(char board[][],int dirt[][],int idn,int idm){
+        board[idn][idm]='m';
+        for(int i=0;i<4;i++){
+            int putRow=idn+dirt[i][0];
+            int putColm=idm+dirt[i][1];
+            if(putRow>=0 && putColm>=0 && putRow<board.length && putColm<board[0].length){
+                if(board[putRow][putColm]=='O'){
+                    fn(board,dirt,putRow,putColm);
+                }
+            }
         }
-        board[i][j]='T';
-        fn(board,i+1,j);
-        fn(board,i,j+1);
-        fn(board,i-1,j);
-        fn(board,i,j-1);
     }
 }
