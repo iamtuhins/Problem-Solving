583619e349432d864bfd792444baaf74e1616608
iamtuhins <77203950+iamtuhins@users.noreply.github.com>
2022-08-21 21:55:39 +0530

    Time: 38 ms (14.03%), Space: 73.2 MB (37.61%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -6,35 +6,41 @@ class Solution {
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
-                    q.add(new Pair(i,j));
-                }else{
-                    mat[i][j]=-1;
+                    q.add(new Pair(i,j,0));
                 }
             }
         }
-        int nums[][]={{-1,0},{0,-1},{1,0},{0,1}};
+        int dirt[][]={{0,1},{1,0},{0,-1},{-1,0}};
+        int vist[][]=new int[n][m];
         while(!q.isEmpty()){
-            Pair p=q.poll();
-            int n1=p.n;
-            int m1=p.m;
-            for(int i=0;i<4;i++){
-                int id1=n1+nums[i][0];
-                int id2=m1+nums[i][1];
-                if(id1<0 || id2<0 || id1>=n || id2>=m ||mat[id1][id2]!=-1){
-                    continue;
+            int sz=q.size();
+            for(int i=0;i<sz;i++){
+                Pair p=q.remove();
+                int nowRow=p.n;
+                int nowColm=p.m;
+                int nums=p.num;
+                mat[nowRow][nowColm]=nums;
+                for(int j=0;j<4;j++){
+                    int putRow=nowRow+dirt[j][0];
+                    int putColm=nowColm+dirt[j][1];
+                    if(putRow>=0 && putColm>=0 && putRow<n && putColm<m && mat[putRow][putColm]==1 && vist[putRow][putColm]==0){
+                        q.add(new Pair(putRow,putColm,nums+1));
+                        vist[putRow][putColm]=1;
+                    }
                 }
-                mat[id1][id2]=mat[n1][m1]+1;
-                q.add(new Pair(id1,id2));
             }
         }
         return mat;
+        
     }
 }
 class Pair{
     int n;
     int m;
-    Pair(int n,int m){
+    int num;
+    Pair(int n,int m,int num){
         this.n=n;
         this.m=m;
+        this.num=num;
     }
 }
