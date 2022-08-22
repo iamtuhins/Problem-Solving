ff150601900e20abce048d6e4dee5879101ffb12
iamtuhins <77203950+iamtuhins@users.noreply.github.com>
2022-08-22 21:15:28 +0530

    Time: 8 ms (21.77%), Space: 42.8 MB (87.93%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.java
+++ 684-redundant-connection/684-redundant-connection.java
@@ -1,26 +1,20 @@
 class Solution {
     public int[] findRedundantConnection(int[][] edges) {
         int n=edges.length;
+        int m=edges[0].length;
         List<List<Integer>>adj=new ArrayList<>();
         for(int i=0;i<=n;i++){
             adj.add(new ArrayList<Integer>());
         }
         int sol[]=new int[2];
-        for(int i=0;i<edges.length;i++){
-            int vist[]=new int[n+1];
+        for(int i=0;i<n;i++){
             int u=edges[i][0];
             int v=edges[i][1];
-            Map<Integer,Integer>map=new HashMap<>();
-            map.put(u,-1);
-            
-            if(u==v){
-                sol[0]=u;
-                sol[1]=v;
-                return sol;
-            }
+            int vists[]=new int[n+1];
             adj.get(u).add(v);
             adj.get(v).add(u);
-            if(dfs(u,adj,vist,map)){
+            boolean temp=fn(adj,u,vists,-1);
+            if(temp){
                 sol[0]=u;
                 sol[1]=v;
                 return sol;
@@ -28,16 +22,17 @@ class Solution {
         }
         return sol;
         
-        
     }
-    boolean dfs(int node,List<List<Integer>>adj,int vist[],Map<Integer,Integer>map){
-        vist[node]=1;
-        for(int n: adj.get(node)){
-            if(vist[n]==1 && map.get(node)!=n){
+    boolean fn(List<List<Integer>>adj,int id,int vists[],int pnt){
+        vists[id]=1;
+        for(int temp: adj.get(id)){
+            if(vists[temp]==0){
+             boolean tm=fn(adj,temp,vists,id);
+                if(tm==true){
+                    return true;
+                }
+            }else if(pnt!=temp){
                 return true;
-            }else if(vist[n]==0){
-                map.put(n,node);
-                dfs(n,adj,vist,map);
             }
         }
         return false;
