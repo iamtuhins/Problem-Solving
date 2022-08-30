178d3a259ac276da8a209803875a5b3cbea3810a
iamtuhins <77203950+iamtuhins@users.noreply.github.com>
2022-08-30 21:37:47 +0530

    Time: 2 ms (82.09%), Space: 49.4 MB (49.88%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,27 +1,24 @@
 class Solution {
     public int jump(int[] nums) {
-        int dp[]=new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            dp[i]=-1;
-        }
-        return fn(nums,0,dp);
-        
-        
-    }
-    
-    int fn(int arr[],int id,int dp[]){
-        if(id>=arr.length-1){
+        int n=nums.length;
+        if(n==1){
             return 0;
         }
-        if(dp[id]>0){
-            return dp[id];
+        int value=0;
+        int base=0;
+        int sol=0;
+        for(int i=0;i<n;i++){
+            value=Math.max(i+nums[i],value);
+            if(value>=n-1){
+                sol++;
+                break;
+            }
+            if(i==base){
+                base=value;
+                sol++;
+            }
         }
-        int sol=101010;
-        for(int i=1;i<=arr[id];i++){
-            int temp=fn(arr,id+i,dp)+1;
-            sol=Math.min(sol,temp);
-        }
-        dp[id]=sol;
         return sol;
+        
     }
 }
