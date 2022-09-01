85ac067d770bbb3fbd4f1e24748340ad0e2b2ff6
iamtuhins <77203950+iamtuhins@users.noreply.github.com>
2022-09-01 21:01:05 +0530

    Time: 1 ms (89.88%), Space: 54 MB (35.71%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,33 +1,35 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
         int n=graph.length;
-        int colorGraph[]=new int[n];
-        for(int i=0;i<n;i++)
-            colorGraph[i]=-1;
+        int nodes[]=new int[n];
         for(int i=0;i<n;i++){
-            if(colorGraph[i]==-1){
-                colorGraph[i]=1;
-                boolean temp=fn(graph,i,colorGraph);
-                if(temp==false){
+            nodes[i]=-1;
+        }
+        nodes[0]=-1;
+        boolean sol=true;
+        for(int i=0;i<n;i++){
+            if(nodes[i]==-1){
+                nodes[i]=0;
+                boolean temp=fn(graph,i,-1,nodes);
+                if(temp==false)
                     return false;
-                }
             }
         }
-        return true;
+        return sol;
+        
         
     }
-    boolean fn(int graph[][],int id,int arr[]){
-        for(int num: graph[id]){
-            if(arr[num]==-1){
-                arr[num]=1-arr[id];
-                boolean temp=fn(graph,num,arr);
-                if(temp==false){
-                    return false;
-                }
-            }else if(arr[num]==arr[id]){
-                return false;
-            }
-        }
+    boolean fn(int graph[][],int id,int parent,int nodes[]){
+       for(int nm: graph[id]){
+           if(nodes[nm]==-1){
+               nodes[nm]=1-nodes[id];
+               boolean temp=fn(graph,nm,id,nodes);
+               if(temp==false)
+                   return false;
+           }else if(nm!=parent && nodes[nm]==nodes[id]){
+               return false;
+           }
+       }
         return true;
     }
 }
