d61301b1016abc48c6db4f6fb077ee5f083a7f29
iamtuhins <77203950+iamtuhins@users.noreply.github.com>
2022-09-08 21:01:47 +0530

    Time: 1 ms (99.93%), Space: 41.8 MB (94.59%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
@@ -1,29 +1,30 @@
 class Solution {
     public int[] numsSameConsecDiff(int n, int k) {
-        List<Integer>lst=new ArrayList<>();
-        for(int i=1;i<10;i++){
-            fn(i,1,n,k,i,lst);
+        List<Integer>temp=new ArrayList<>();
+        for(int i=1;i<=9;i++){
+            fn(i,temp,1,k,i,n);
         }
-        int sol[]=new int[lst.size()];
-        for(int i=0;i<sol.length;i++){
-            sol[i]=lst.get(i);
+        int sol[]=new int[temp.size()];
+        int size=temp.size();
+        for(int i=0;i<size;i++){
+            sol[i]=temp.get(i);
         }
         return sol;
         
     }
-    void fn(int digit,int nums,int n,int k,int prev,List<Integer>lst){
-        if(nums==n){
-            lst.add(digit);
+    void fn(int num,List<Integer>temp,int n,int k,int prev,int now){
+        if(n==now){
+            temp.add(num);
             return;
         }
         int next=prev+k;
-        if(next<=9){
-            fn((digit*10)+next,nums+1,n,k,next,lst);
+        if(next<10){
+            fn((num*10)+next,temp,n+1,k,next,now);
         }
         next=prev-k;
         if(k!=0 && next>=0){
-            fn((digit*10)+next,nums+1,n,k,next,lst);
+            fn((num*10)+next,temp,n+1,k,next,now);
         }
-        
+            
     }
 }
