70cc582553cad110f7226ea77d79abe91eec5d98
iamtuhins <77203950+iamtuhins@users.noreply.github.com>
2022-09-10 12:28:11 +0530

    Time: 7 ms (31.13%), Space: 50.5 MB (96.35%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,51 +1,44 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int n=grid.length;
         int sol=0;
-        int m=grid[0].length;
-        int vist[][]=new int[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]=='1' && vist[i][j]==0){
-                    fn(grid,vist,i,j);
+        int vst[][]=new int[grid.length][grid[0].length];
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(vst[i][j]==0 && grid[i][j]=='1'){
+                    fn(grid,vst,i,j);
                     sol++;
                 }
             }
-            
         }
         return sol;
         
     }
-    void fn(char grid[][],int vist[][],int n,int m){
+    void fn(char [][]grid,int [][]vist,int n,int m){
         Queue<Pair>q=new LinkedList<>();
-        int dirt[][]={{0,1},{1,0},{0,-1},{-1,0}};
         q.add(new Pair(n,m));
         vist[n][m]=1;
+        int dirt[][]={{0,1},{1,0},{0,-1},{-1,0}};
         while(!q.isEmpty()){
-            int sz=q.size();
-            for(int i=0;i<sz;i++){
-                Pair temp=q.remove();
-                int idn=temp.n;
-                int idm=temp.m;
-                for(int j=0;j<4;j++){
-                    int putN=idn+dirt[j][0];
-                    int putM=idm+dirt[j][1];
-                    if(putN>=0 && putM>=0 && putN<grid.length && putM<grid[0].length){
-                        if(vist[putN][putM]==0 && grid[putN][putM]=='1'){
-                            q.add(new Pair(putN,putM));
-                            vist[putN][putM]=1;
-                        }
-                    }
+            Pair p=q.remove();
+            int idn=p.idn;
+            int idm=p.idm;
+            for(int i=0;i<dirt.length;i++){
+                int putN=idn+dirt[i][0];
+                int putM=idm+dirt[i][1];
+                if(putN>=0 && putM>=0 && putN<grid.length && putM<grid[0].length && vist[putN][putM]==0 && grid[putN][putM]=='1'){
+                    q.add(new Pair(putN,putM));
+                    vist[putN][putM]=1;
+                    
                 }
             }
         }
     }
 }
 class Pair{
-    int n;
-    int m;
+    int idn;
+    int idm;
     Pair(int n,int m){
-        this.n=n;
-        this.m=m;
+        this.idn=n;
+        this.idm=m;
     }
 }
