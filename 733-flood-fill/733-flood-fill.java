b2ac47db473f92fc183c803aa5e4578b1fe03978
iamtuhins <77203950+iamtuhins@users.noreply.github.com>
2022-09-10 12:14:47 +0530

    Time: 2 ms (31.52%), Space: 48 MB (58.74%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -1,38 +1,22 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int n=image.length;
-        int m=image[0].length;
-        int dist[][]={{1,0},{0,1},{-1,0},{0,-1}};
-        int startValue=image[sr][sc];
-        Queue<Pair>q=new LinkedList<>();
-        q.add(new Pair(sr,sc));
-        image[sr][sc]=color;
-        while(!q.isEmpty()){
-            int sz=q.size();
-            for(int i=0;i<sz;i++){
-                Pair temp=q.remove();
-                for(int j=0;j<4;j++){
-                    int idn=temp.n;
-                    int idm=temp.m;
-                    idn=idn+dist[j][0];
-                    idm=idm+dist[j][1];
-                    if(idn>=0 && idm>=0 && idn<n && idm<m && image[idn][idm]==startValue && startValue!=color){
-                        q.add(new Pair(idn,idm));
-                        image[idn][idm]=color;
-                    }
-                }
-            }
-        }
+        int dirt[][]={{0,1},{1,0},{0,-1},{-1,0}};
+        int vt[][]=new int[image.length][image[0].length];
+        fn(image,sr,sc,color,vt,dirt);
         return image;
         
-        
     }
-}
-class Pair{
-    int n;
-    int m;
-    Pair(int n1,int m1){
-        this.n=n1;
-        this.m=m1;
-    }
+   void fn(int [][]arr,int idN,int idM,int color,int vist[][],int dirt[][]){
+       int temp=arr[idN][idM];
+       arr[idN][idM]=color;
+       vist[idN][idM]=1;
+       for(int i=0;i<dirt.length;i++){
+           int putN=idN+dirt[i][0];
+           int putM=idM+dirt[i][1];
+           if(putN>=0 && putM>=0 && putN<arr.length && putM<arr[0].length && vist[putN][putM]==0 && arr[putN][putM]==temp){
+               fn(arr,putN,putM,color,vist,dirt);
+           }
+           
+       }
+   }
 }
