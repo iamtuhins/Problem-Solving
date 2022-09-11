cfd77a6fda593f13b52e640a8ed6df2106170ec4
iamtuhins <77203950+iamtuhins@users.noreply.github.com>
2022-09-11 19:57:09 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (59.48%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,26 +1,23 @@
 class Solution {
     public int climbStairs(int n) {
-        int []nums=new int[n+1];
-        int m=0;
-        int sol=fn(n,nums,m);
+        int map[]=new int[n+1];
+        Arrays.fill(map,-1);
+        int sol=fn(n,0,map);
         return sol;
         
         
-        
     }
-    int fn(int n,int nums[],int idn){
-        if(idn>n){
+    int fn(int n,int nums,int[]map){
+        if(nums>n){
             return 0;
         }
-        if(nums[idn]>0){
-            return nums[idn];
-        }
-        if(idn==n){
+        if(n==nums){
             return 1;
         }
-        int lf=fn(n,nums,idn+1);
-        int rt=fn(n,nums,idn+2);
-        nums[idn]=lf+rt;
-        return lf+rt;
+        if(map[nums]!=-1){
+            return map[nums];
+        }
+        map[nums]=fn(n,nums+1,map)+fn(n,nums+2,map);
+        return map[nums];
     }
 }
