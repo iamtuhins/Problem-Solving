4084929457363a6e15a06e458d78bf08ca6344d1
iamtuhins <77203950+iamtuhins@users.noreply.github.com>
2022-09-25 20:44:51 +0530

    Time: 10 ms (22.96%), Space: 48.1 MB (58.33%) - LeetHub

--- 622-design-circular-queue/622-design-circular-queue.java
+++ 622-design-circular-queue/622-design-circular-queue.java
@@ -1,64 +1,65 @@
 class MyCircularQueue {
-
-    private int front;
-    private int rear;
-    private int[] arr;
-    private int cap;
-    
-    private int next(int i){ // to get next idx after i in circular queue
-        return (i+1)%cap;
-    }
-    private int prev(int i){ // to get prev idx before i in circular queue
-        return (i+cap-1)%cap;
-    }
-    
-	// rest is as simple as implmenting a normal queue using array.
+    int sz=0;
+    List<Integer>ls;
     public MyCircularQueue(int k) {
-        arr = new int[k];
-        cap=k;
-        front=-1;
-        rear=-1;
+        this.sz=k;
+        ls=new ArrayList<>();
     }
     
     public boolean enQueue(int value) {
-        if(isFull())return false;
-        if(front==-1){
-            front=0;
-            rear=0;
-            arr[rear]=value;
-            return true;
+        if(ls.size()>=sz){
+            return false;
         }
-        rear = next(rear);
-        arr[rear]=value;
-        return true;
+        return ls.add(value);
     }
     
     public boolean deQueue() {
-        if(isEmpty())return false;
-        if(front==rear){
-            front=-1;
-            rear=-1;
-            return true;
+        if(ls.size()==0){
+            return false;
         }
-        front=next(front);
+        ls.remove(0);
         return true;
     }
     
     public int Front() {
-        if(front==-1)return -1;
-        return arr[front];
+        int temp=ls.size();
+        if(temp==0){
+            return -1;
+        }
+        return ls.get(0);
     }
     
     public int Rear() {
-        if(rear==-1)return -1;
-        return arr[rear];
+        int temp=ls.size();
+        if(temp==0){
+            return -1;
+        }
+       int num=ls.get(temp-1);
+        return num;
     }
     
     public boolean isEmpty() {
-        return front==-1;
+        if(ls.size()==0){
+            return true;
+        }
+        return false;
     }
     
     public boolean isFull() {
-        return front!=-1 && next(rear)==front;
+        if(ls.size()==sz){
+            return true;
+        }
+        return false;
     }
 }
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue obj = new MyCircularQueue(k);
+ * boolean param_1 = obj.enQueue(value);
+ * boolean param_2 = obj.deQueue();
+ * int param_3 = obj.Front();
+ * int param_4 = obj.Rear();
+ * boolean param_5 = obj.isEmpty();
+ * boolean param_6 = obj.isFull();
+ */
