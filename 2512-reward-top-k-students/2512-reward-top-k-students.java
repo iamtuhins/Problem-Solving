0942a7e3361f2d5749857ee8b63d5cf191abcebd
Tuhin Sarkar <77203950+iamtuhins@users.noreply.github.com>
2022-12-29 21:43:24 +0530

    Time: 84 ms (49.70%), Space: 50.6 MB (43.50%) - LeetHub

--- 2512-reward-top-k-students/2512-reward-top-k-students.java
+++ 2512-reward-top-k-students/2512-reward-top-k-students.java
@@ -1,25 +1,26 @@
 class Solution {
     public List<Integer> topStudents(String[] positive_feedback, String[] negative_feedback, String[] report, int[] student_id, int k) {
-        Set<String>nSet=new HashSet<>(Arrays.asList(positive_feedback));
-        Set<String>mSet=new HashSet<>(Arrays.asList(negative_feedback));
-        int arrs[][]=new int[report.length][2];
+        Set<String>pSet=new HashSet<>(Arrays.asList(positive_feedback));
+        Set<String>nSet=new HashSet<>(Arrays.asList(negative_feedback));
+        int temp[][]=new int[report.length][2];
         for(int i=0;i<report.length;i++){
-            String temp[]=report[i].split(" ");
+            String arr[]=report[i].split(" ");
             int score=0;
-            for(String mn: temp){
-                if(nSet.contains(mn)){
+            for(String n: arr){
+                if(pSet.contains(n)){
                     score+=3;
-                }else if(mSet.contains(mn)){
+                }else if(nSet.contains(n)){
                     score--;
                 }
             }
-            arrs[i][0]=student_id[i];
-            arrs[i][1]=score;
+            //y
+            temp[i][0]=student_id[i];
+            temp[i][1]=score;
         }
-        Arrays.sort(arrs,(a,b)->a[1]==b[1]? Integer.compare(a[0],b[0]): Integer.compare(b[1],a[1]));
+        Arrays.sort(temp,(a,b)->a[1]==b[1]?Integer.compare(a[0],b[0]):Integer.compare(b[1],a[1]));
         List<Integer>sol=new ArrayList<>();
         for(int i=0;i<k;i++){
-            sol.add(arrs[i][0]);
+            sol.add(temp[i][0]);
         }
         return sol;
         
