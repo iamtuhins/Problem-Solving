85c526c93469739a19ab7133dc13ba9d8bd1ed08
Tuhin Sarkar <77203950+iamtuhins@users.noreply.github.com>
2023-02-03 16:16:05 +0530

    Time: 183 ms (24.75%), Space: 210.1 MB (42.58%) - LeetHub

--- 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.java
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.java
@@ -1,31 +1,30 @@
 class Solution {
     public int minSideJumps(int[] obstacles) {
-        int dp[][]=new int[4][obstacles.length];
-        for(int i=0;i<4;i++){
+        int dp[][]=new int[3+1][obstacles.length];
+        for(int i=0;i<=3;i++){
             Arrays.fill(dp[i],-1);
         }
         int n=fn(obstacles,2,0,dp);
         return n;
         
     }
-    int fn(int []obstacles,int currlane,int currpos,int [][]dp){
-        if(currpos==obstacles.length-1){
+    int fn(int arr[],int lane,int pos,int [][]dp){
+        if(pos==arr.length-1){
             return 0;
         }
-        if(dp[currlane][currpos]!=-1){
-            return dp[currlane][currpos];
+        if(dp[lane][pos]!=-1){
+            return dp[lane][pos];
         }
-        if(obstacles[currpos+1]!=currlane){
-            dp[currlane][currpos]= fn(obstacles,currlane,currpos+1,dp);
-            return dp[currlane][currpos];
+        if(arr[pos+1]!=lane){
+            return fn(arr,lane,pos+1,dp);
         }
-        int mz=Integer.MAX_VALUE;
+        int min=Integer.MAX_VALUE;
         for(int i=1;i<=3;i++){
-            if(i!=currlane && obstacles[currpos]!=i){
-                mz=Math.min(mz,1+fn(obstacles,i,currpos,dp));
+            if(i!=lane && arr[pos]!=i){
+                min=Math.min(min,1+fn(arr,i,pos,dp));
             }
         }
-        dp[currlane][currpos]=mz;
-        return mz;
+        dp[lane][pos]=min;
+        return min;
     }
 }
