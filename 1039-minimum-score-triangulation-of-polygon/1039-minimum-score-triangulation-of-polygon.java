68b46f24c6b9a505ce8975db3bc012b9929fe24a
Tuhin Sarkar <77203950+iamtuhins@users.noreply.github.com>
2023-02-28 12:07:38 +0530

    Time: 2 ms (100.00%), Space: 39.8 MB (84.43%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.java
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.java
@@ -2,24 +2,25 @@ class Solution {
     public int minScoreTriangulation(int[] values) {
         int n=values.length;
         int dp[][]=new int[n+1][n+1];
-        for(int i=0;i<n;i++){
+        for(int i=0;i<=n;i++){
             Arrays.fill(dp[i],-1);
         }
         int m=fn(values,0,n-1,dp);
         return m;
+        
     }
-    int fn(int []arr,int n,int m,int dp[][]){
-        if(n+1==m){
+    int fn(int arr[],int i, int j,int [][]dp){
+        if(i+1==j){
             return 0;
         }
-        if(dp[n][m]!=-1){
-            return dp[n][m];
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
-        int sol=Integer.MAX_VALUE;
-        for(int k=n+1;k<m;k++){
-            sol=Math.min(sol,arr[n]*arr[k]*arr[m]+fn(arr,n,k,dp)+fn(arr,k,m,dp));
+        int temp=Integer.MAX_VALUE;
+        for(int k=i+1;k<j;k++){
+            temp=Math.min(temp,arr[i]*arr[j]*arr[k]+fn(arr,i,k,dp)+fn(arr,k,j,dp));
         }
-        dp[n][m]=sol;
-        return dp[n][m];
+        dp[i][j]=temp;
+        return temp;
     }
 }
