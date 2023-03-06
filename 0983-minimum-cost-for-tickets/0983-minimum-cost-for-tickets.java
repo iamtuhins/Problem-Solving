9e47669c56a5417231e9323df109dd6af4949bdc
Tuhin Sarkar <77203950+iamtuhins@users.noreply.github.com>
2023-03-06 12:21:04 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (75.18%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,26 +1,26 @@
 class Solution {
     public int mincostTickets(int[] days, int[] costs) {
         int n=days.length;
-        int dp[]=new int[n];
+        int dp[]=new int[n+1];
         Arrays.fill(dp,-1);
-        int m=fn(n,days,costs,0,dp);
+        int m=fn(days,costs,n,0,dp);
         return m;
     }
-    int fn(int n,int days[],int []cost,int id,int dp[]){
+    int fn(int []days,int cost[],int n,int id,int[] dp){
         if(id>=n){
             return 0;
         }
+        int i=0;
         if(dp[id]!=-1){
             return dp[id];
         }
-        int oneDayPass=cost[0]+fn(n,days,cost,id+1,dp);
-        int i=0;
+        int oneDay=cost[0]+fn(days,cost,n,id+1,dp);
         for(i=id;i<n && days[i]<days[id]+7;i++);
-        int sevenDaypass=cost[1]+fn(n,days,cost,i,dp);
+        int sevenDays=cost[1]+fn(days,cost,n,i,dp);
         for(i=id;i<n && days[i]<days[id]+30;i++);
-        int thdDayPass=cost[2]+fn(n,days,cost,i,dp);
-        int sol=Math.min(oneDayPass,Math.min(sevenDaypass,thdDayPass));
-        dp[id]=sol;
-        return dp[id];
+        int ThirtyDays=cost[2]+fn(days,cost,n,i,dp);
+        int temp=Math.min(oneDay,Math.min(sevenDays,ThirtyDays));
+        dp[id]=temp;
+        return temp;
     }
 }
