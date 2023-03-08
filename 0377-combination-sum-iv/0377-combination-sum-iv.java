0cc413f04a2afaa0507dd3a1c646d85ad7b999cf
Tuhin Sarkar <77203950+iamtuhins@users.noreply.github.com>
2023-03-08 13:01:28 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (33.12%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -3,26 +3,26 @@ class Solution {
         int map[]=new int[target+1];
         Arrays.fill(map,-1);
         int m=fn(target,nums,map);
-        return m; 
+        return m;
         
     }
-    int fn(int target,int nums[],int []map){
+    int fn(int target,int arr[],int []map){
         if(target==0){
             return 1;
         }
-        if(0>target){
+        if(target<0){
             return 0;
         }
         if(map[target]!=-1){
             return map[target];
         }
         int temp=0;
-        for(int i=0;i<nums.length;i++){
-            target=target-nums[i];
-            temp=temp+fn(target,nums,map);
-            target=target+nums[i];
+        for(int i=0;i<arr.length;i++){
+            target-=arr[i];
+            temp=temp+fn(target,arr,map);
+            target+=arr[i];
         }
         map[target]=temp;
-        return map[target];
+        return temp;
     }
 }
