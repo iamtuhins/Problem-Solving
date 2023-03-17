584653a00f480c512e5bd05a1ec7f006879d2c59
Tuhin Sarkar <77203950+iamtuhins@users.noreply.github.com>
2023-03-17 12:51:44 +0530

    Time: 201 ms (42.79%), Space: 43.1 MB (17.17%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,19 +1,26 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        int m=fn(nums,0,target);
+        int n=nums.length;
+        Map<String,Integer>dp=new HashMap<>();
+        int m=fn(nums,0,target,dp);
         return m;
     }
-    int fn(int []arr,int id,int target){
+    int fn(int []arr,int id,int target,Map<String,Integer>dp){
         if(id>=arr.length){
             if(target==0){
                 return 1;
             }
             return 0;
         }
+        String key=id+"-"+target;
+        if(dp.containsKey(key)){
+            return dp.get(key);
+        }
         int nums=arr[id]*-1;
-        int getting=fn(arr,id+1,target-(nums));
-        int notGetting=fn(arr,id+1,target-arr[id]);
+        int getting=fn(arr,id+1,target-(nums),dp);
+        int notGetting=fn(arr,id+1,target-arr[id],dp);
         int temp=getting+notGetting;
+        dp.put(id+"-"+target,temp);
         return temp;
     }
 }
