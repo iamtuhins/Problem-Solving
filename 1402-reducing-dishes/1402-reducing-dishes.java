6f9231724c67edebade1add154a522623383aa0c
Tuhin Sarkar <77203950+iamtuhins@users.noreply.github.com>
2023-05-07 20:37:02 +0530

    Time: 21 ms (9.14%), Space: 42.6 MB (22.39%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.java
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -1,26 +1,29 @@
 class Solution {
     public int maxSatisfaction(int[] satisfaction) {
+        int m=satisfaction.length;
         Arrays.sort(satisfaction);
-        int n=satisfaction.length;
-        int dp[][]=new int[n+1][n+1];
-        for(int i=0;i<dp.length;i++){
-            Arrays.fill(dp[i],-1); 
+        int id=0;
+        int dp[][]=new int[m+1][m+1];
+        for(int i=0;i<=m;i++){
+            Arrays.fill(dp[i],-1);
         }
-        int m=fn(satisfaction,0,1,dp);
-        return m;
+        int n=fn(satisfaction,id,1,dp);
+        return n;
         
     }
-    int fn(int arr[],int id, int num,int [][]dp){
+    int fn(int arr[],int id,int val,int[][]dp){
         if(id>=arr.length){
             return 0;
         }
-        if(dp[id][num]!=-1){
-            return dp[id][num];
+        if(dp[id][val]!=-1){
+            return dp[id][val];
         }
-        int adding=(arr[id]*num)+fn(arr,id+1,num+1,dp);
-        int notAdding=0+fn(arr,id+1,num,dp);
-        int temp=Math.max(adding,notAdding);
-        dp[id][num]=temp;
-        return temp;
+        int adding=0;
+        int notAdding=0;
+        adding=arr[id]*val+(fn(arr,id+1,val+1,dp));
+        notAdding=0+fn(arr,id+1,val,dp);
+        int sol=Math.max(adding,notAdding);
+        dp[id][val]=sol;
+        return sol;
     }
 }
