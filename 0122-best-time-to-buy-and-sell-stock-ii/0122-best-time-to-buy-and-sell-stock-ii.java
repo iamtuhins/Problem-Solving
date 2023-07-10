a280d6290894f1f70e7efa50a6a7f349f60a143a
Tuhin Sarkar <77203950+iamtuhins@users.noreply.github.com>
2023-07-10 20:56:02 +0530

    Time: 5 ms (7.15%), Space: 46.9 MB (5.61%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,35 +1,33 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n=0;
-        int buy=1;
         int m=prices.length;
+        int buy=0;
         int dp[][]=new int[m+1][2];
         for(int i=0;i<dp.length;i++){
             Arrays.fill(dp[i],-1);
         }
-        n=fn(prices,0,buy,dp);
+        int n=fn(prices,0,buy,dp);
         return n;
         
-        
     }
-    int fn(int arr[],int id, int buy,int [][]dp){
+    int fn(int []arr,int id,int buy,int [][]dp){
         if(id>=arr.length){
             return 0;
         }
         if(dp[id][buy]!=-1){
             return dp[id][buy];
         }
-        int profit=0;
-        if(buy==1){
-            int temp1= (-1*arr[id])+fn(arr,id+1,0,dp);
-            int temp2=0+fn(arr,id+1,1,dp);
-            profit=Math.max(temp1,temp2);
-        }else{
-            int temp1=arr[id]+fn(arr,id+1,1,dp);
+        int n=0;
+        if(buy==0){
+            int temp1=-arr[id]+fn(arr,id+1,1,dp);
             int temp2=0+fn(arr,id+1,0,dp);
-            profit=Math.max(temp1,temp2);
+            n=Math.max(temp1,temp2);
+        }else{
+            int temp1=arr[id]+fn(arr,id+1,0,dp);
+            int temp2=0+fn(arr,id+1,1,dp);
+            n=Math.max(temp1,temp2);
         }
-        dp[id][buy]=profit;
-        return profit;
+        dp[id][buy]=n;
+        return n;
     }
 }
