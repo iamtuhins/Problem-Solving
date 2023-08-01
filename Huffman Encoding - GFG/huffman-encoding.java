0f6eb7053540f03523a3e3e875da549e3dc1f43f
Tuhin Sarkar <77203950+iamtuhins@users.noreply.github.com>
2023-08-01 21:45:01 +0530

    Added solution - LeetHub

---
+++ Huffman Encoding - GFG/huffman-encoding.java
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            String S = br.readLine().trim();
+            int N = S.length();
+            Solution ob = new Solution();
+            String[] element = br.readLine().trim().split("\\s+");
+		    int[] f = new int[N];
+		    for(int i = 0; i < N; i++){
+		        f[i] = Integer.parseInt(element[i]);
+		    }
+            ArrayList<String> res  = ob.huffmanCodes(S,f,N);
+            for(int i = 0; i < res.size(); i++)
+            {
+                System.out.print(res.get(i)+" ");
+            }
+            System.out.println();
+        }
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    public ArrayList<String> huffmanCodes(String S, int f[], int N)
+    {
+        // Code here
+        Cm Comparators=new Cm();
+        PriorityQueue<Node>pq=new PriorityQueue<>(Comparators);
+        for(int i=0;i<f.length;i++){
+            Node nd=new Node(f[i]);
+            pq.add(nd);
+        }
+        while(pq.size()!=1){
+            Node num1=pq.poll();
+            Node num2=pq.poll();
+            Node NodeNew=new Node(num1.data+num2.data);
+            NodeNew.left=num1;
+            NodeNew.right=num2;
+            pq.add(NodeNew);
+        }
+        ArrayList<String> sol=new ArrayList<>();
+        Node fst=pq.poll();
+        traverse(fst,"",sol);
+        return sol;
+        
+    }
+     void traverse(Node nd,String temp, ArrayList<String>lst){
+        if(nd.left==null && nd.right==null){
+            lst.add(temp);
+            return;
+        }
+        traverse(nd.left,temp+'0',lst);
+        traverse(nd.right,temp+'1',lst);
+    }
+}
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data=data;
+        left=null;
+        right=null;
+    }
+}
+class Cm implements Comparator<Node>{
+    public int compare(Node n1,Node n2){
+       if(n1.data==n2.data){
+           return 1;
+       }else{
+           return n1.data-n2.data;
+       }
+    }
+}
